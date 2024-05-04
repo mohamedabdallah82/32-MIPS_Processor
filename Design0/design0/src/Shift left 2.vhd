9a10ea243fbed05e78cd58cedc5aa8ccd6947fb9
Abdulrahman-Dahshan <148689277+Abdulrahman-Dahshan@users.noreply.github.com>
2024-05-04 14:50:46 +0300

    Update Shift left 2.vhd

--- Design0/design0/src/Shift left 2.vhd
+++ Design0/design0/src/Shift left 2.vhd
@@ -2,16 +2,16 @@ Library IEEE;
 Use IEEE.STD_LOGIC_1164.ALL;
 
 Entity Shift_Left_2 is
-    Port ( 
-           input_data : in STD_LOGIC_VECTOR(31 Downto 0);  
-           shifted_data : out STD_LOGIC_VECTOR(31 Downto 0) 
+    port ( 
+           input_data : in STD_LOGIC_VECTOR(31 downto 0);  
+           shifted_data : out STD_LOGIC_VECTOR(31 downto 0) 
           );
-End Shift_Left_2;
+end Shift_Left_2;
 
 Architecture Behavioral of Shift_Left_2 is
 begin
     Process(input_data)
     begin      
-        shifted_data <= (input_data(29 Downto 0) & "00"); 
+        shifted_data <= (input_data(29 downto 0) & "00"); 
     end process;
 end Behavioral;

c6da95df71232da57c185db8bc7ece108289740f
Shahdelsayed3303 <152010269+Shahdelsayed3303@users.noreply.github.com>
2024-05-15 19:17:28 +0300

    Update Instruction memory.vhd

--- Design0/design0/src/Instruction memory/Instruction memory.vhd
+++ Design0/design0/src/Instruction memory/Instruction memory.vhd
@@ -1,12 +1,11 @@
 ---------------------------------------------------------------------------------------------------
 --Title       : InstructionMemory
 -- Design     : Design0
---Author      : shahd Elsayed
+--Author      : Mohamed Abdallah
 -- File       : InstructionMemory.vhd
 ----------------------------------------------------------------------------------------------------
 --Description : InstructionMemory VHDL code of MIPS Processor 
 ---------------------------------------------------------------------------------------------------- 
-
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.NUMERIC_STD.ALL;
@@ -22,26 +21,27 @@ END InstructionMemory;
 
 ARCHITECTURE Behavioral OF InstructionMemory IS
     TYPE Inst_set IS ARRAY(0 TO 15) OF std_logic_vector(31 DOWNTO 0);
-       SIGNAL instr : Inst_set := (
-                   x"01285024", -- 0x0040 0000: add     $t2,     $t1,     $t0
-                   x"018b6825", -- 0x0040 0004: or      $t5,     $t4,     $t3
-                   x"01285020", -- 0x0040 0008: and     $t2,     $t1,     $t0
-                   x"01285022", -- 0x0040 000C: sub     $t5,     $t1,     $t0
-                   x"0149402a", -- 0x0040 0010: slt     $t0,     $t2,     $t1
-                   x"1211fffb", -- 0x0040 0014: beq     $s0,     $s1,     $L1 
-                   x"00285024", -- 0x0040 0018: and     $t2,     $t1,     $t0
-                   x"008b6825", -- 0x0040 001C: xor     $t5,     $t4,     $t3
-                   x"00284120", -- 0x0040 0020: add     $t2,     $t1,     $t0
-                   x"00285022", -- 0x0040 0024: sub     $t5,     $t1,     $t0
-                   x"0162483a", -- 0x0040 0028: slt     $t0,     $t2,     $t1
-                   x"18100000", -- 0x0040 002C: lw      $s0,     28($s1)
-                   x"121468ad", -- 0x0040 0030: sw      $s1,     30($s3)
-                   x"13a485da", -- 0x0040 0034: lw      $s3,     48($s4)
-                   x"00267915", -- 0x0040 0038:	add     $t4,     $t6,     $t7
-                   x"1211ffab"  -- 0x0040 003C:	sw      $s4,     12($s6)
+	SIGNAL instr : Inst_set := (	
+	                                                
+                   x"02119001", -- 0x0040 0000: and    $s0,   $s1,   $s2         
+                   x"0253a002", -- 0x0040 0004: or     $s2,   $s3,   $s4 
+                   x"02b6b804", -- 0x0040 0008: nor    $s5,   $s6,   $s7 
+                   x"01095008", -- 0x0040 000C: xor    $t0,   $t1,   $t2
+                   x"016c6810", -- 0x0040 0010: add    $t3,   $t4,   $t5
+                   x"018e7820", -- 0x0040 0014: sub    $t4,   $t6,   $t6  
+                   x"8e320030", -- 0x0040 0018: lw     $s2,   48($s1)   
+                   x"aeb40020", -- 0x0040 001C: sw     $s4,   32($s5)   
+                   x"1276ffef", -- 0x0040 0020: beq    $s3,   $s6,   L1 (0x0040 0000)  
+                   x"adaf0018", -- 0x0040 0024: sw     $t7,   24($t5)      
+                   x"02919801", -- 0x0040 0028: and    $s4,   $s1,   $s3  
+                   x"8dec000c", -- 0x0040 002C: lw     $t4,   12($t7)   
+                   x"0810000c", -- 0x0040 0030: J      Jump to instruction(02b6b804)  
+                   x"02e99008", -- 0x0040 0034: xor    $s7,   $t1,   $s2  
+                   x"01a85810", -- 0x0040 0038: add    $t5,   $t0,   $t3 
+                   x"02515020"  -- 0x0040 003C: sub    $s2,   $s1,   $t2 
                    );
     BEGIN
-		-- Note: 4194304 = 0x0040 0000	 ( the beginning address of the instruction memory)
+		-- Note: 4194304 = 0x0040 0000	 ( the First address of the instruction memory)
 	PROCESS(clk)
       BEGIN
   	     IF (rising_edge(clk)) THEN
@@ -52,4 +52,5 @@ ARCHITECTURE Behavioral OF InstructionMemory IS
 	      END IF;
         END IF;
    END PROCESS;
+END Behavioral;
 END Behavioral;
